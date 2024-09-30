f93082f1457e332ba8142025b252cbeb991fbe23
pandora-lab <filthydrms@gmail.com>
2024-09-30 08:57:19 +0000

    corrected string

--- vars/artifact_download_nexus.groovy
+++ vars/artifact_download_nexus.groovy
@@ -1,4 +1,4 @@
-def call(Srting repo_Url, String repo_Username, String repo_Password, String target_Artifact_Name) {
+def call(String repo_Url, String repo_Username, String repo_Password, String target_Artifact_Name) {
     println("cloning Repo")
     sh "curl -u repo_Username:repo_Password -O ${nexusUrl}"
     sh "ls -al"
