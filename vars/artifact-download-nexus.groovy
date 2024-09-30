fd3e1ad2f1a2783dd30d1efcf9c0fa411ca46f16
pandora-lab <filthydrms@gmail.com>
2024-09-30 08:41:54 +0000

    artifact download

---
+++ vars/artifact-download-nexus.groovy
@@ -0,0 +1,5 @@
+def call(Srting repo_Url, String repo_Username, String repo_Password, String target_Artifact_Name) {
+    println("cloning Repo")
+    sh "curl -u repo_Username:repo_Password -O ${nexusUrl}"
+    sh "ls -al"
+}
