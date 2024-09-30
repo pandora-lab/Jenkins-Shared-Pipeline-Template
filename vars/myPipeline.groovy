b9dc0691dfcc4e038ea265593abd8bbeb200f5a5
pandora-lab <filthydrms@gmail.com>
2024-09-30 10:54:38 +0530

    Update myPipeline.groovy

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -1,31 +1,4 @@
 // vars/myPipeline.groovy
 def call(Map config) {
-    pipeline {
-        agent any
-        stages {
-            stage('Build') {
-                steps {
-                    echo 'Building...'
-                   // sh 'mvn clean install' // Example build step
-                }
-            }
-            stage('Test') {
-                steps {
-                    echo 'Running tests...'
-                    // sh 'mvn test' // Example test step
-                }
-            }
-            stage('Deploy') {
-                steps {
-                    echo 'Deploying...'
-                    // Example deployment logic
-                    if (config.env == 'production') {
-                       // sh 'deploy-prod.sh'
-                    } else {
-                       // sh 'deploy-dev.sh'
-                    }
-                }
-            }
-        }
-    }
+    echo "Hello Gents and ladies"
 }
