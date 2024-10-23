abb15862fba9966e45cae9c7a3517012fd9b43ac
pandora-lab <filthydrms@gmail.com>
2024-10-23 08:17:48 +0000

    test

--- vars/helloworld.groovy
+++ vars/helloworld.groovy
@@ -1,12 +1,10 @@
 def call(String name) {
     stage('Hello World') {
-        steps {
             script {
                 // Print Python version
                 sh "python3 --version"
                 // Execute the Python script
                 sh "python3 scripts/helloworld.py ${name}"
             }
-        }
     }
 }
