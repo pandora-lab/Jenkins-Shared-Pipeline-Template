2c3f2794120870dd081ce0c23b8b83407bfdcd5b
pandora-lab <filthydrms@gmail.com>
2024-11-08 08:23:13 +0000

    test

--- vars/fullpipeline.groovy
+++ vars/fullpipeline.groovy
@@ -1,22 +1,20 @@
+// vars/myPipeline.groovy
 def call() {
-    pipeline {
-        agent any
-        stages {
-            stage('Build') {
-                steps {
-                    echo 'Building...'
-                }
+    return [
+        stage('Build') {
+            steps {
+                echo 'Building...'
             }
-            stage('Test') {
-                steps {
-                    echo 'Testing...'
-                }
+        },
+        stage('Test') {
+            steps {
+                echo 'Testing...'
             }
-            stage('Deploy') {
-                steps {
-                    echo 'Deploying...'
-                }
+        },
+        stage('Deploy') {
+            steps {
+                echo 'Deploying...'
             }
         }
-    }
+    ]
 }
