c26fd3b7977ed9b50e9c007b4b73d6b01a367988
pandora-lab <filthydrms@gmail.com>
2024-11-08 08:16:43 +0000

    test

--- vars/fullpipeline.groovy
+++ vars/fullpipeline.groovy
@@ -1,23 +1,23 @@
 def call() {
-    stages {
-        stage('Building Distributable Package') {
-            steps {
-                echo 'Building'
-            }
+
+    stage('Building Distributable Package') {
+        steps {
+            echo 'Building'
         }
-        stage('Archiving Package') {
-            steps {
-                echo 'Archiving Aritfacts'
-                archiveArtifacts artifacts: '/*.zip', fingerprint: true
-            }
+    }
+    stage('Archiving Package') {
+        steps {
+            echo 'Archiving Aritfacts'
+            archiveArtifacts artifacts: '/*.zip', fingerprint: true
         }
-        stage('Deploying Dev') {
-            steps {
-                echo 'Deploying'
-                timeout(time:3, unit:'DAYS') {
-                    input message: "Approve build?"
-                }
+    }
+    stage('Deploying Dev') {
+        steps {
+            echo 'Deploying'
+            timeout(time:3, unit:'DAYS') {
+                input message: "Approve build?"
             }
         }
     }
+
 }
