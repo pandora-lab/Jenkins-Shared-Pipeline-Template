b675b6a54f0286b45d3bf47227751374c8e4efcc
pandora-lab <filthydrms@gmail.com>
2024-11-15 08:53:41 +0000

    test

--- vars/secondary_pipeline.groovy
+++ vars/secondary_pipeline.groovy
@@ -1,26 +1,24 @@
 def call() {
-    pipeline {
-        agent any
-        stages {
-            stage('DEPLOY FILES') {
-                steps {
-                    script {
-                        echo "DEPLOYING FILES"
-                    }
+
+    stages {
+        stage('DEPLOY FILES') {
+            steps {
+                script {
+                    echo "DEPLOYING FILES"
                 }
             }
-            stage('BUILD FILES') {
-                steps {
-                    script {
-                        echo "BUILDING FILES"
-                    }
+        }
+        stage('BUILD FILES') {
+            steps {
+                script {
+                    echo "BUILDING FILES"
                 }
             }
-            stage('COPY FILES') {
-                steps {
-                    script {
-                        echo "BUILDING FILES"
-                    }
+        }
+        stage('COPY FILES') {
+            steps {
+                script {
+                    echo "BUILDING FILES"
                 }
             }
         }
